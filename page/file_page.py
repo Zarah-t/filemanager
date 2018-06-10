bb6aa4000eacafb482617f4f6696ce35311bcd88
hitheima <hitheima@163.com>
2018-06-10 14:50:01 +0800

    增加侧边栏和点击事件

--- page/file_page.py
+++ page/file_page.py
@@ -17,6 +17,8 @@ class FilePage(BaseAction):
     book_mark_button = By.XPATH, "书签"
     # 快捷方式
     shot_cut_button = By.XPATH, "快捷方式"
+    # 侧边栏
+    side_menu_button = By.ID, "android:id/home"
 
     def click_menu(self):
         self.click(self.menu_button)
@@ -29,4 +31,9 @@ class FilePage(BaseAction):
 
     def click_shot_cut(self):
         self.click(self.shot_cut_button)
+
+    def click_side_menu(self):
+        self.click(self.side_menu_button)
+
+
 
