ccb9cb6538e3a4a3086681db9db3a695ffa55cbd
hitheima <hitheima@163.com>
2018-06-10 14:53:54 +0800

    增加tes函数名

--- scripts/test_file.py
+++ scripts/test_file.py
@@ -1,6 +1,17 @@
 import os, sys
 sys.path.append(os.getcwd())
 
+
 class TestFile:
 
-    def test_(self):
+    def test_refresh(self):
+        pass
+
+    def test_book_mark(self):
+        pass
+
+    def test_shot_cut(self):
+        pass
+
+    def test_set_as_home(self):
+        pass
